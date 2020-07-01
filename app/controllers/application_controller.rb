b256878036d37629ee137609225df1dac8b42a5a
vagrant <vagrant@localhost.localdomain>
2020-07-01 05:59:28 +0000

    [update]meshiterro_review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    # 登録・ログイン認証をする前（before_actionで）に、「configure_permitted_parameters」が実行される。
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    # protected　は他の呼び出されたコントローラ（deviseのコントローラ）から操作できる。/ private　は他のコントローラからは参照できないから今回は使ってない。
+    protected
+    def configure_permitted_parameters
+        # そもそも、「name」のカラムは、deviseのデフォルトでは存在しないため、カラムを追加する際には、以下のような書き方で追記が必要。
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        # sign_upの時に、:nameの操作を許可（permit）している。
+    end
+    
 end
--- db/migrate/20200701044609_devise_create_users.rb
+++ db/migrate/20200701044609_devise_create_users.rb
@@ -32,6 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      # ユーザ登録の名前の欄を追加するため、「t.string :name」のカラムを追加する
       t.string :name
       t.timestamps null: false
     end
